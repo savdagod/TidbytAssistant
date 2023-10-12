5fcd5fc5e8030f66dacf8d4f2cc9d9eda7c25fc7
savdagod <107087485+savdagod@users.noreply.github.com>
2023-10-12 16:37:43 -0400

    Update TidbytDisplay.sh

--- TidbytDisplay.sh
+++ TidbytDisplay.sh
@@ -1,24 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-CONTENT=${1:?"missing arg 1 for CONTENT"}
-DEVICE=${2:?"missing arg 2 for DEVICE"}
+# These will be sent directly from HomeAssistant
+CONTENT=${1:?"missing arg 1 for CONTENT"} 
+TB_DEVICEID=${2:?"missing arg 2 for DEVICE ID"}
+TB_TOKEN=${3:?"missing arg 3 for TOKEN"}
 
-case $DEVICE in
-	livingroom)
-		TB_DEVICEID=""
-		TB_TOKEN=""
-	;;
-
-	loft)
-		TB_DEVICE=""
-		TB_TOKEN=""
-	;;
-esac
-
-ROOT_DIR=~/pixlet_apps/TidbytDisplay
+ROOT_DIR=/usr/share/hassio/homeassistant/TidbytDisplay #this is wherever you put the folder
 RENDER_PATH=$ROOT_DIR/render.webp
 
 pixlet render $ROOT_DIR/display/$CONTENT.star -o $RENDER_PATH
-		
+
 pixlet push --api-token $TB_TOKEN $TB_DEVICEID $RENDER_PATH
+
 exit 0
