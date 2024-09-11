eefb249920cad9d73c026c97967d9415a2e20044
Bruce <107087485+savdagod@users.noreply.github.com>
2024-09-10 22:58:52 -0400

    Add files via upload

---
+++ custom_components/tidbytassistant/const.py
@@ -0,0 +1,10 @@
+DOMAIN = "tidbytassistant"
+
+CONF_ID = "deviceid"
+CONF_TOKEN = "token"
+CONF_PORT = "port"
+CONF_HOST = "host"
+CONF_NAME = "name"
+CONF_DEVICE = "device"
+ATTR_CONTENT = "content"
+ATTR_DEVICENANME = "devicename"
