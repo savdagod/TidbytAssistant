d7d2485d473a940e51bf65a96e13f726a2192886
Bruce <107087485+savdagod@users.noreply.github.com>
2024-09-12 18:25:46 -0400

    Update to support new service
    Add new service that adds pushed app to the rotation of apps, rather than pushing once.

--- custom_components/tidbytassistant/__init__.py
+++ custom_components/tidbytassistant/__init__.py
@@ -1,7 +1,7 @@
 import logging
 import requests
 from typing import Dict
-from .const import DOMAIN, CONF_DEVICE, CONF_TOKEN, CONF_ID, CONF_PORT, CONF_NAME, ATTR_CONTENT, ATTR_DEVICENANME, ATTR_CONT_TYPE, ATTR_CUSTOM_CONT
+from .const import DOMAIN, CONF_DEVICE, CONF_TOKEN, CONF_ID, CONF_PORT, CONF_NAME, ATTR_CONTENT, ATTR_CONTENT_ID, ATTR_DEVICENANME, ATTR_CONT_TYPE, ATTR_CUSTOM_CONT
 from homeassistant.core import HomeAssistant, ServiceCall
 from homeassistant.helpers.typing import ConfigType
 from homeassistant.exceptions import HomeAssistantError
@@ -71,8 +71,37 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
                 response = requests.post(url, json=todo)
             except:
                 raise HomeAssistantError(f"Could not communicate with the add-on. Is it installed?")
-
+    
+    def pixlet_publish(call: ServiceCall) -> None:
+        """Handle the service action call."""
+        
+        url = f"http://localhost:{port}/hooks/tidbyt-publish"
+        content = call.data.get(ATTR_CONTENT)
+        contentid = call.data.get(ATTR_CONTENT_ID)
+        devicename = call.data.get(ATTR_DEVICENANME)
+        data = config[DOMAIN]
+        devicefound = False
+        for item in data[CONF_DEVICE]:
+            if item[CONF_NAME] == devicename:
+                token = item[CONF_TOKEN]
+                deviceid = item[CONF_ID]
+                devicefound = True
+                break
+        if devicefound is False:
+            valid_names = ""
+            for item in data[CONF_DEVICE]:
+                vname = item[CONF_NAME]
+                valid_names += vname + ", "
+            valid_names = valid_names[:-2]
+            raise HomeAssistantError(f"Device name {devicename} was not found. Valid device names are: {valid_names}")
+        else:  
+            todo = {"content": content, "contentid": contentid, "token": token, "deviceid": deviceid}
+            try:
+                response = requests.post(url, json=todo)
+            except:
+                raise HomeAssistantError(f"Could not communicate with the add-on. Is it installed?")
     hass.services.register(DOMAIN, "Push", pixlet_push)
+    hass.services.register(DOMAIN, "Publish", pixlet_publish)
 
     # Return boolean to indicate that initialization was successful.
     return True
